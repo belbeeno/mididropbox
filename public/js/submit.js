00083aa1d1e663feb9e06055285d1805bdc6cc98
belbeeno <640716+belbeeno@users.noreply.github.com>
2022-12-28 23:57:59 +0000

    some bootstrapper / fe stuff with submit

--- public/js/submit.js
+++ public/js/submit.js
@@ -1,9 +1,23 @@
 var form = document.getElementById("form");
 form.addEventListener("submit", submitForm);
+var txtName = document.getElementById("textName");
+var frmFile = document.getElementById("formFile");
+txtName.addEventListener('change', refreshCanSubmit);
+frmFile.addEventListener('change', refreshCanSubmit);
+refreshCanSubmit();
 function canSubmit() {
     var txtName = document.getElementById("textName");
     var frmFile = document.getElementById("formFile");
-    return txtName && frmFile.files.length >= 1;
+    return txtName && txtName.value && frmFile.files.length >= 1;
+}
+function refreshCanSubmit() {
+    var btnSubmit = document.getElementById("btnSubmit");
+    if (canSubmit()) {
+        btnSubmit.removeAttribute('disabled');
+    }
+    else {
+        btnSubmit.setAttribute('disabled', '');
+    }
 }
 function submitForm(e) {
     e.preventDefault();
@@ -12,5 +26,5 @@ function submitForm(e) {
     //const formData = new FormData();
     //formData.append("name", txtName.value);
     //formData.append("file", frmFile.files[0]);
-    console.log("submitting name " + txtName.value + " file " + frmFile.files[0].name);
+    console.log('submitting name: \"' + txtName.value + '\" file: [' + frmFile.files[0].name + ']');
 }
--- public/js/submit.ts
+++ public/js/submit.ts
@@ -1,10 +1,25 @@
 const form = document.getElementById("form");
 form.addEventListener("submit", submitForm);
+const txtName : HTMLInputElement = document.getElementById("textName") as HTMLInputElement;
+const frmFile : HTMLInputElement = document.getElementById("formFile") as HTMLInputElement;
+txtName.addEventListener('change', refreshCanSubmit);
+frmFile.addEventListener('change', refreshCanSubmit);
+refreshCanSubmit();
 
 function canSubmit() : boolean {
-    const txtName = document.getElementById("textName");
+    const txtName : HTMLInputElement = document.getElementById("textName") as HTMLInputElement;
     const frmFile : HTMLInputElement = document.getElementById("formFile") as HTMLInputElement;
-    return txtName && frmFile.files.length >= 1;
+    return txtName && txtName.value && frmFile.files.length >= 1;
+}
+
+function refreshCanSubmit() {
+    const btnSubmit = document.getElementById("btnSubmit");
+    if (canSubmit()) {
+        btnSubmit.removeAttribute('disabled');
+    }
+    else {
+        btnSubmit.setAttribute('disabled', '');
+    }
 }
 
 function submitForm(e : Event) {
@@ -15,5 +30,5 @@ function submitForm(e : Event) {
     //formData.append("name", txtName.value);
     //formData.append("file", frmFile.files[0]);
 
-    console.log("submitting name " + txtName.value + " file " + frmFile.files[0].name);
+    console.log('submitting name: \"' + txtName.value + '\" file: [' + frmFile.files[0].name + ']');
 }
